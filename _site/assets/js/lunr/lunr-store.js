3af06ac9516079da67e7d9b073416577cc982abb
ubermen5che <ubermen5ch1308@gmail.com>
2020-12-20 12:55:06 +0900

    update my blog

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First post  Yong",
+        "excerpt":"2020년 12월 20일 블로그 첫번째 포스팅.  앞으로 공부하면서 부딫힌 문제들을 해결하고 이곳에 기록할 것이다. 이번 겨울방학 동안 다룰 첫번째 주제로는 Deep-learning이다. 최대한 시간을 내어 내용을 잘 정리해서 포스팅하고자한다.      블록 인용할거야         인용구지롱       _ 코드를 이쁘게 표현 _  int val = 10; printf(%s,\"Hello, World!\");      deeplearning   web   jekyll   markdown   Google     ","categories": [],
+        "tags": [],
+        "url": "https://ubermen5che.github.io/First-Post-yong/",
+        "teaser": null
+      }]
