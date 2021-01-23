f28a242e4bade76ba6c19a851a641abf7957592c
ubermen5che <ubermen5ch1308@gmail.com>
2021-01-23 16:48:01 +0900

    전체 commit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,12 +1,12 @@
 var store = [{
         "title": "First post  Yong",
-        "excerpt":"2020년 12월 20일 블로그 첫번째 포스팅.  앞으로 공부하면서 부딫힌 문제들을 해결하고 이곳에 기록할 것이다. 이번 겨울방학 동안 다룰 첫번째 주제로는 Deep-learning이다. 최대한 시간을 내어 내용을 잘 정리해서 포스팅하고자한다.      블록 인용할거야         인용구지롱       _ 코드를 이쁘게 표현 _  int val = 10; printf(%s,\"Hello, World!\");      deeplearning   web   jekyll   markdown   Google     ","categories": [],
+        "excerpt":"2020년 12월 20일 블로그 첫번째 포스팅.  앞으로 공부하면서 부딫힌 문제들을 해결하고 이곳에 기록할 것입니다. 또한 관심분야에 대한 포스팅도 이어나갈 예정입니다!  ","categories": [],
         "tags": [],
         "url": "https://ubermen5che.github.io/First-Post-yong/",
         "teaser": null
       },{
         "title": "딥러닝 - 퍼셉트론(perceptron)",
-        "excerpt":"이번 글에서는 퍼셉트론 알고리즘에 대해 알아보는 시간을 가지도록 하겠습니다. 퍼셉트론은 신경망(딥러닝)의 기원이 되는 알고리즘이기 때문에 퍼셉트론의 구조를 이해하는 것은 딥러닝을 배우는데에 있어서 중요한 아이디어를 배우는 일입니다.   퍼셉트론이란?   퍼셉트론은 다수의 신호를 입력으로 받아 하나의 신호를 출력합니다.                [그림 1-1] 입력이 2개인 퍼셉트론   ","categories": ["AI","Deep-Learning"],
+        "excerpt":"이번 글에서는 퍼셉트론 알고리즘에 대해 알아보겠습니다. 퍼셉트론은 신경망(딥러닝)의 기원이 되는 알고리즘이기 때문에 퍼셉트론의 구조를 이해하는 것은 딥러닝을 배우는데에 있어서 중요한 아이디어를 배우는 일입니다. 퍼셉트론이란? 퍼셉트론은 다수의 신호를 입력으로 받아 하나의 신호를 출력합니다. 아래 그림[1-1]은 2개의 신호를 받은 퍼셉트론의 예를 보여줍니다. \\(x_1\\)과 \\(x_2\\)는 입력 신호, \\(y\\)는 출력 신호, \\(w_1\\)과 \\(w_2\\)는 가중치를...","categories": ["AI","Deep-Learning"],
         "tags": ["perceptron","AI","Deep-Learning"],
         "url": "https://ubermen5che.github.io/ai/deep-learning/perceptron/",
         "teaser": null
@@ -21,5 +21,11 @@ var store = [{
         "excerpt":"What is an arguments? 이전 포스팅에서 논리학은 적절하게 정리된 생각에 관한 과학이라고 했다. 모든과학분야에서 해당 연구분야에 대한 초점과 목표를 가지는데, 논리학에서의 목표는 논쟁이다. 논쟁은 우리의 생각을 구조화하는 방법이다 우리는 가끔 말싸움을 하며 논쟁을 벌이기도 한다. 만약 친구, 동료, 가족들과 논쟁을 벌이는거라면 어떤일에 대해 논쟁의 대상자의 의견에 동의하지않거나 본인이 주장하는 의견과...","categories": ["Logic","Computer Science"],
         "tags": ["Logic","Computer Science","논리","컴퓨터과학"],
         "url": "https://ubermen5che.github.io/logic/computer%20science/Logic-and-Computer-Science-Logic-Arguments/",
+        "teaser": null
+      },{
+        "title": "Logic and Computer Science - 논리학 기초 - Statements",
+        "excerpt":"What is an statements? statements는 참 또는 참일 가능성이 있는 문장이다. statements를 만들때 어떤 참또는 거짓을 보일 수 있는 문장을 만들어야한다. 모든 한국시민들은 한국에 살고 있다. 서울에 있는 모든 기업들은 사무실에서 레드 카펫을 사용하고있다. 애플은 마이크로소프트보다 더 많은 자본을 가지고 있다. 위의 예를 잘 살펴보면, 각 statements는 참을 나타내려고 시도하고...","categories": ["Logic","Computer Science"],
+        "tags": ["Logic","Computer Science","논리","컴퓨터과학"],
+        "url": "https://ubermen5che.github.io/logic/computer%20science/Logic-and-Computer-Science-Logic-Statements/",
         "teaser": null
       }]
