f8c89f3be2f38eb6d9500b150832af5a831dc38c
ubermen5che <ubermen5ch1308@gmail.com>
2022-01-05 21:36:03 +0900

    Add post baekjoon 뱀 3190

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,144 +2,150 @@ var store = [{
         "title": "First post  Yong",
         "excerpt":"2020년 12월 20일 블로그 첫번째 포스팅.  앞으로 공부하면서 부딫힌 문제들을 해결하고 이곳에 기록할 것입니다. 또한 관심분야에 대한 포스팅도 이어나갈 예정입니다!  ","categories": [],
         "tags": [],
-        "url": "https://ubermen5che.github.io/First-Post-yong/",
+        "url": "http://localhost:4000/First-Post-yong/",
         "teaser": null
       },{
         "title": "딥러닝 - 퍼셉트론(perceptron)",
         "excerpt":"이번 글에서는 퍼셉트론 알고리즘에 대해 알아보겠습니다. 퍼셉트론은 신경망(딥러닝)의 기원이 되는 알고리즘이기 때문에 퍼셉트론의 구조를 이해하는 것은 딥러닝을 배우는데에 있어서 중요한 아이디어를 배우는 일입니다. 퍼셉트론이란? 퍼셉트론은 다수의 신호를 입력으로 받아 하나의 신호를 출력합니다. 아래 그림[1-1]은 2개의 신호를 받은 퍼셉트론의 예를 보여줍니다. \\(x_1\\)과 \\(x_2\\)는 입력 신호, \\(y\\)는 출력 신호, \\(w_1\\)과 \\(w_2\\)는 가중치를...","categories": ["AI","Deep-Learning"],
         "tags": ["perceptron","AI","Deep-Learning"],
-        "url": "https://ubermen5che.github.io/ai/deep-learning/perceptron/",
+        "url": "http://localhost:4000/ai/deep-learning/perceptron/",
         "teaser": null
       },{
         "title": "Logic and Computer Science - 논리학 기초 - 논리",
         "excerpt":"What is Logic? 논리(Logic)란 올바른 추론의 규칙 체계이다. 논리는 논증이 올바른 형식을 가지고 있는지 아닌지 결정하는데 도움을 준다. 이 때 올바른 추론(right reasoning)이란 말에 다음과 같은 의문점을 가질 수 있다. 어떤 누군가가 무엇이 옳고 그름을 정한건가요? 논리는 단지 어떤 나이들어 이미 이 세상에 존재하지않는 어떤 사람이 발명해낸것이 아닌가요? 왜 그...","categories": ["Logic","Computer Science"],
         "tags": ["Logic","Computer Science","논리","컴퓨터과학"],
-        "url": "https://ubermen5che.github.io/logic/computer%20science/Logic-and-Computer-Science-Logic-An-Introduction-to-Logic/",
+        "url": "http://localhost:4000/logic/computer%20science/Logic-and-Computer-Science-Logic-An-Introduction-to-Logic/",
         "teaser": null
       },{
         "title": "Logic and Computer Science - 논리학 기초 - 논쟁(argument)",
         "excerpt":"What is an arguments? 이전 포스팅에서 논리학은 적절하게 정리된 생각에 관한 과학이라고 했다. 모든과학분야에서 해당 연구분야에 대한 초점과 목표를 가지는데, 논리학에서의 목표는 논쟁이다. 논쟁은 우리의 생각을 구조화하는 방법이다 우리는 가끔 말싸움을 하며 논쟁을 벌이기도 한다. 만약 친구, 동료, 가족들과 논쟁을 벌이는거라면 어떤일에 대해 논쟁의 대상자의 의견에 동의하지않거나 본인이 주장하는 의견과...","categories": ["Logic","Computer Science"],
         "tags": ["Logic","Computer Science","논리","컴퓨터과학"],
-        "url": "https://ubermen5che.github.io/logic/computer%20science/Logic-and-Computer-Science-Logic-Arguments/",
+        "url": "http://localhost:4000/logic/computer%20science/Logic-and-Computer-Science-Logic-Arguments/",
         "teaser": null
       },{
         "title": "Logic and Computer Science - 논리학 기초 - Statements",
         "excerpt":"What is an statements? statements는 참 또는 참일 가능성이 있는 문장이다. statements를 만들때 어떤 참또는 거짓을 보일 수 있는 문장을 만들어야한다. 모든 한국시민들은 한국에 살고 있다. 서울에 있는 모든 기업들은 사무실에서 레드 카펫을 사용하고있다. 애플은 마이크로소프트보다 더 많은 자본을 가지고 있다. 위의 예를 잘 살펴보면, 각 statements는 참을 나타내려고 시도하고...","categories": ["Logic","Computer Science"],
         "tags": ["Logic","Computer Science","논리","컴퓨터과학"],
-        "url": "https://ubermen5che.github.io/logic/computer%20science/Logic-and-Computer-Science-Logic-Statements/",
+        "url": "http://localhost:4000/logic/computer%20science/Logic-and-Computer-Science-Logic-Statements/",
         "teaser": null
       },{
         "title": "Linear algebra Vector",
         "excerpt":"선형대수 기본 벡터 Linear Algebra - Essence of linear algebra(Vector) 벡터 개념의 필요성 최근 정보검색, 데이터과학기초(Fundementals of data science)과목을 수강하면서 벡터의 중요성을 깨닫게 되었습니다. 정보검색에서 document와 query의 similarity를 측정하기 위해 벡터가 필요하였고, 데이터과학기초에서는 Supervised learning 범주에 속하는 SVM(Support Vector Machine)에서 벡터 개념이 필요하였습니다. 지난학기 벡터 개념이 제대로 정립이 되어있지 않아...","categories": [],
         "tags": [],
-        "url": "https://ubermen5che.github.io/Linear-algebra-Vector/",
+        "url": "http://localhost:4000/Linear-algebra-Vector/",
         "teaser": null
       },{
         "title": "자바 기초 정리",
         "excerpt":"Local variable: method내에 정의된 변수. local이라고 이름이 붙혀진 이유는 함수 정의 안에서 한정되기 때문이다. 두 메소드가 존재한다고 가정했을 때, 서로 다른 메소드 내에 같은 변수이름이 존재해도 상관없다. 그리고 둘중 하나의 변수명을 변경했다고 해서 다른 변수에 전혀 영향을 주지 않는다.(지역적) Object(instance of the class): 클래스의 오브젝트 또는 클래스의 인스턴스라고도 불려진다. 오브젝트는...","categories": ["Java"],
         "tags": ["Java"],
-        "url": "https://ubermen5che.github.io/java/Java-%EA%B8%B0%EC%B4%88-%EC%A0%95%EB%A6%AC/",
+        "url": "http://localhost:4000/java/Java-%EA%B8%B0%EC%B4%88-%EC%A0%95%EB%A6%AC/",
         "teaser": null
       },{
         "title": "[대외활동] 삼성 SDS 멘토링",
         "excerpt":"2021년 6월 29일 화요일 운이좋게도 삼성 SDS랜선 멘토링에 참여하게되었습니다. 오늘 강의해주셨던 내용들을 단순 기억에 의존하여 작성한 내용이니 참고해주시면 좋을것 같습니다. 주된내용은 좋은개발이 무엇인지, 개발자에 대한 덕목에 대한 내용입니다. 개발의 시작은 두가지 - 다른 사람으로 부터 오는 요구사항을 개발. - 나의 생각에 따른 개발. 개발자에게 가장 중요한 덕목 개발자는 어떠한 문제가...","categories": ["대외활동"],
         "tags": ["삼성SDS","멘토링","프로그래머","개발자"],
-        "url": "https://ubermen5che.github.io/%EB%8C%80%EC%99%B8%ED%99%9C%EB%8F%99/%EC%82%BC%EC%84%B1SDS-%EB%9E%9C%EC%84%A0-%EB%A9%98%ED%86%A0%EB%A7%81/",
+        "url": "http://localhost:4000/%EB%8C%80%EC%99%B8%ED%99%9C%EB%8F%99/%EC%82%BC%EC%84%B1SDS-%EB%9E%9C%EC%84%A0-%EB%A9%98%ED%86%A0%EB%A7%81/",
         "teaser": null
       },{
         "title": "[Web] 스프링 웹 개발 기초",
         "excerpt":"[Web] 스프링 웹 개발 기초 정적 컨텐츠 간단히 말해서 요청이 들어올 시 서버에서 웹 브라우저에 파일을 전송해주면 웹브라우저는 파일을 받아서 그대로 보여주는 방식 스프링에서 정적 컨텐츠는 project에 /static 경로를 찾아서 클라이언트에게 뿌려줌. 웹브라우저가 정적 페이지를 요청을 하면, Spring 내장 Tomcat서버를 거쳐 우선 스프링 컨테이너 안에 해당 요청에 관한 컨트롤러가 있는지...","categories": ["Java","Spring"],
         "tags": ["Java","MVC","API","템플릿 엔진","Template engine"],
-        "url": "https://ubermen5che.github.io/java/spring/Web-%EC%8A%A4%ED%94%84%EB%A7%81-%EC%9B%B9-%EA%B0%9C%EB%B0%9C-%EA%B8%B0%EC%B4%88/",
+        "url": "http://localhost:4000/java/spring/Web-%EC%8A%A4%ED%94%84%EB%A7%81-%EC%9B%B9-%EA%B0%9C%EB%B0%9C-%EA%B8%B0%EC%B4%88/",
         "teaser": null
       },{
         "title": "[스프링 핵심 원리] SOLID ",
         "excerpt":"SRP: 단일 책임 원칙(Single responsibility principle) 한 클래스는 하나의 책임만 가져야 한다. 하나의 책임이라는 것은 모호하다. 클 수 있고, 작을 수 있다. 문맥과 상황에 따라 다르다. 중요한 기준은 변경이다. 변경이 있을 때 파급 효과가 적으면 단일 책임 원칙을 잘 따른 것 예) UI변경, 객체의 생성과 사용을 분리 ⭐️OCP: 개방-폐쇄 원칙(Open/closed...","categories": ["Java","Spring"],
         "tags": ["Java","Spring","SOLID"],
-        "url": "https://ubermen5che.github.io/java/spring/%EC%9E%90%EB%B0%94-%EC%8A%A4%ED%94%84%EB%A7%81-SOLID/",
+        "url": "http://localhost:4000/java/spring/%EC%9E%90%EB%B0%94-%EC%8A%A4%ED%94%84%EB%A7%81-SOLID/",
         "teaser": null
       },{
         "title": "대구시 어린이 교통사고와 어린이 보호구역의 연관성",
         "excerpt":"Folium을 이용한 데이터 시각화   시각화 자료 정보  - 마커로 표시된 위치는 2013년도부터 2020년도 까지의 대구시 어린이 교통사고 발생 위치를 나타냄. - 클릭시 세부정보 확인 가능. - 노란색 원은 어런이 보호구역을 나타냄. - 원의 반지름은 200M로 설정하였음.           ","categories": ["Datavisualization"],
         "tags": ["Folium"],
-        "url": "https://ubermen5che.github.io/datavisualization/The-relationship-between-child-traffic-accidents-and-child-protection-areas-in-Daegu/",
+        "url": "http://localhost:4000/datavisualization/The-relationship-between-child-traffic-accidents-and-child-protection-areas-in-Daegu/",
         "teaser": null
       },{
         "title": "[Spring] 싱글톤 컨테이너",
         "excerpt":"웹 어플리케이션과 싱글톤 웹 어플리케이션은 보통 수많은 클라이언트가 동시에 요청을 한다. 만약 클라이언트의 요청마다 새로운 객체(Controller)를 생성해서 서비스하게 된다면 JVM에 수많은 객체가 등록되고 결국 OOM(Out Of Memory)가 발생하여 정상적으로 서비스를 할 수 없게 될것이다. 이를 해결하기 위해 나온것이 싱글톤 컨테이너이다. 그림1(DI 컨테이너) 그림1은 스프링 없는 순수한 DI 컨테이너인 AppConfig가 요청을...","categories": ["Spring","Design pattern"],
         "tags": ["Spring","singleton"],
-        "url": "https://ubermen5che.github.io/spring/design%20pattern/Spring-%EC%8B%B1%EA%B8%80-%ED%86%A4-%EC%BB%A8%ED%85%8C%EC%9D%B4%EB%84%88/",
+        "url": "http://localhost:4000/spring/design%20pattern/Spring-%EC%8B%B1%EA%B8%80-%ED%86%A4-%EC%BB%A8%ED%85%8C%EC%9D%B4%EB%84%88/",
         "teaser": null
       },{
         "title": "[컨트리뷰톤] Git/GitHub 기본실습-1",
         "excerpt":"오픈소스 프로젝트 복사 / 다운 받기(Fork &amp; Clone) 우선 Fork하는 행위에 대한 의미를 알아보자. Fork를 이해하려면 우선 commit의 기본개념을 알아야한다. 커밋이란? 소스파일의 변화분이다. 그림1(커밋 로그) Fork는 Fork한 프로젝트의 commit내역을 모두 복사한것과 동일하다고 생각하면 쉽다. 즉 내가 fork한 프로젝트와는 별개의 복사된 프로젝트라고 보면된다. 복사된 프로젝트이니 당연히 fork한 프로젝트를 수정한다고 해서 원본...","categories": ["Git","대외 활동"],
         "tags": ["Git/Github","git 명령"],
-        "url": "https://ubermen5che.github.io/git/%EB%8C%80%EC%99%B8%20%ED%99%9C%EB%8F%99/%EC%BB%A8%ED%8A%B8%EB%A6%AC%EB%B7%B0%ED%86%A4-Git-%EA%B8%B0%EB%B3%B8%EC%8B%A4%EC%8A%B5-1/",
+        "url": "http://localhost:4000/git/%EB%8C%80%EC%99%B8%20%ED%99%9C%EB%8F%99/%EC%BB%A8%ED%8A%B8%EB%A6%AC%EB%B7%B0%ED%86%A4-Git-%EA%B8%B0%EB%B3%B8%EC%8B%A4%EC%8A%B5-1/",
         "teaser": null
       },{
         "title": "[컨트리뷰톤] 오픈소스 협업을 위한 Git/Github 고급과정-1",
         "excerpt":"Rebase의 필요성 평소 개인프로젝트를 할때 당연히 혼자만 commit을 하게된다. 또한 학교에서 팀프로젝트를 하게되는 경우 다수의 팀원들과 git을 이용하여 프로젝트를 진행하지만 모두가 push권한을 가지고 하는경우가 많은것 같다. 이는 협업이 아니며 분업이라고 해야할 것이다. 오픈소스 프로젝트 특성상 다수의 사람이 PR을 날리게 된다. 만약 내가 수정하고 있는 모듈과 다른사람이 수정한 모듈에 연관관계가 있는경우,...","categories": ["Git","대외 활동"],
         "tags": ["Git/Github","git 명령어","rebase","오픈소스"],
-        "url": "https://ubermen5che.github.io/git/%EB%8C%80%EC%99%B8%20%ED%99%9C%EB%8F%99/%EC%BB%A8%ED%8A%B8%EB%A6%AC%EB%B7%B0%ED%86%A4-Git-%EA%B3%A0%EA%B8%89%EC%8B%A4%EC%8A%B5-1/",
+        "url": "http://localhost:4000/git/%EB%8C%80%EC%99%B8%20%ED%99%9C%EB%8F%99/%EC%BB%A8%ED%8A%B8%EB%A6%AC%EB%B7%B0%ED%86%A4-Git-%EA%B3%A0%EA%B8%89%EC%8B%A4%EC%8A%B5-1/",
         "teaser": null
       },{
         "title": "[컨트리뷰톤] Git/GitHub 기본실습-2",
         "excerpt":"오픈소스 개발참여를 위한 commit 작업(branch &amp; commit) Branch란? 브랜치는 다음과 같은 표현으로 간결하게 설명할 수 있다. “같은 폴더 다른세상” 아래의 터미널을 보면 check-fosslight branch에서 hi.txt를 생성하고 commit을 한것을 볼 수 있다. 해당 브랜치에서 생성하고 commit한 파일을 main branch에서 찾아볼 수 있을까? 지난 포스팅에서 배운 git log 명령어를 이용하여 commit내역을 보면...","categories": ["Git","대외 활동"],
         "tags": ["Git/Github","git 명령어","rebase","오픈소스","branch"],
-        "url": "https://ubermen5che.github.io/git/%EB%8C%80%EC%99%B8%20%ED%99%9C%EB%8F%99/%EC%BB%A8%ED%8A%B8%EB%A6%AC%EB%B7%B0%ED%86%A4-Git-%EA%B8%B0%EB%B3%B8%EC%8B%A4%EC%8A%B5-2/",
+        "url": "http://localhost:4000/git/%EB%8C%80%EC%99%B8%20%ED%99%9C%EB%8F%99/%EC%BB%A8%ED%8A%B8%EB%A6%AC%EB%B7%B0%ED%86%A4-Git-%EA%B8%B0%EB%B3%B8%EC%8B%A4%EC%8A%B5-2/",
         "teaser": null
       },{
         "title": "[JAVA] Privacy Leaks",
         "excerpt":"클래스를 정의할 때 외부에서 접근하지 못하도록 instance variables를 private로 둔다. 그러나 private로 설정한다고 해서 privacy leaks을 막을 수 없다. 클래스를 잘못 설계하면 private으로 설정한 instance variable도 값이 변경될 수 있기 때문이다. 잘못 설계된 클래스 예 public class Person(){ private String name; private Date born; private Date died; //생성자 //setter //getter...","categories": ["JAVA","Spring"],
         "tags": ["private","JAVA","Spring"],
-        "url": "https://ubermen5che.github.io/java/spring/JAVA-Privacy-Leaks/",
+        "url": "http://localhost:4000/java/spring/JAVA-Privacy-Leaks/",
         "teaser": null
       },{
         "title": "[Spring] 다양한 의존관계 주입 방법",
         "excerpt":"의존관계 주입은 크게 4가지 방법이 있다 생성자 주입 수정자 주입(setter 주입) 필드 주입 일반 메서드 주입 생성자 주입 생성자를 통해서 의존 관계를 주입 받는 방법이다. 특징 생성자 호출시점에서 딱 1번만 호출되는 것이 보장된다. 불편, 필수 의존관계에 사용 ex) MVC패턴에서 Repository, Service등과 같이 필수적으로 사용되는 객체들 거의 대부분의 서비스에서 생성자 주입방법을...","categories": ["Spring"],
         "tags": ["Spring","DI"],
-        "url": "https://ubermen5che.github.io/spring/Spring-%EB%8B%A4%EC%96%91%ED%95%9C-%EC%9D%98%EC%A1%B4%EA%B4%80%EA%B3%84-%EC%A3%BC%EC%9E%85-%EB%B0%A9%EB%B2%95/",
+        "url": "http://localhost:4000/spring/Spring-%EB%8B%A4%EC%96%91%ED%95%9C-%EC%9D%98%EC%A1%B4%EA%B4%80%EA%B3%84-%EC%A3%BC%EC%9E%85-%EB%B0%A9%EB%B2%95/",
         "teaser": null
       },{
         "title": "[JAVA] Binding",
         "excerpt":"Binding이란 Method invocation과 method definition을 연관짓는 process. Binding 종류 Early binding : 컴파일 타임에 binding이 일어남. Late binding : Run Time에 binding이 일어남. How late binding works? Note 설명하기 쉽도록 가정을 해보도록 하자. 우선 Figure라는 부모 클래스가 존재하고 자식클래스로는 Circle, Square, Triangle.. 여러 도형이 있을 수 있다고 가정한다. 그리고 Figure...","categories": ["JAVA"],
         "tags": ["Binding","Late binding","Early binding","Dynamic binding","Polymorphism"],
-        "url": "https://ubermen5che.github.io/java/JAVA-Binding/",
+        "url": "http://localhost:4000/java/JAVA-Binding/",
         "teaser": null
       },{
         "title": "[Spirng]빈 생명주기 콜백",
         "excerpt":"데이터베이스 커넥션 풀이나, 네트워크 소켓처럼 애플리케이션 시작 시점에 필요한 연결을 미리 해두고, 애플리케이션 종료 시점에 연결을 모두 종료하는 작업을 진행하려면, 객체의 초기화와 종료 작업이 필요하다. 이번 글에서는 스프링을 통해 이러한 초기화 작업과 종료 작업을 어떻게 진행하는지 예제로 알아보고자 한다. 참고: 애플리케이션 시작 시점에 DB 커넥션 풀과 연결을 미리 해두는 이유는...","categories": ["JAVA","Spring"],
         "tags": ["Bean","Bean Lifecycle","Callback"],
-        "url": "https://ubermen5che.github.io/java/spring/spring-bean-lifecycle-callback/",
+        "url": "http://localhost:4000/java/spring/spring-bean-lifecycle-callback/",
         "teaser": null
       },{
         "title": "[System Programming]Connection Control Studying-stty",
         "excerpt":"Ideas and Skills Files과 devices의 유사점 Files과 devices의 차이점 connections에 대한 속성 Race condition과 atomic operations device drivers를 제어하는방법 Streams 5.2 Devices Are Just Like Files Unix시스템에서 모든 devices는 file로써 다루어진다. device도 file과 마찬가지로 filename, inode number, owner, set of permission bits, last modifed time의 정보를 가지고 있다. 우리가 알고있는...","categories": ["Unix/Linux","System Programming","kernel"],
         "tags": ["C","System Programming","stty"],
-        "url": "https://ubermen5che.github.io/unix/linux/system%20programming/kernel/SystemProgramming-Connection-Control-Studying-stty/",
+        "url": "http://localhost:4000/unix/linux/system%20programming/kernel/SystemProgramming-Connection-Control-Studying-stty/",
         "teaser": null
       },{
         "title": "[PS-programmers] 스킬트리",
         "excerpt":"문제 설명 선행 스킬이란 어떤 스킬을 배우기 전에 먼저 배워야 하는 스킬을 뜻합니다. 예를 들어 선행 스킬 순서가 스파크 → 라이트닝 볼트 → 썬더일때, 썬더를 배우려면 먼저 라이트닝 볼트를 배워야 하고, 라이트닝 볼트를 배우려면 먼저 스파크를 배워야 합니다. 위 순서에 없는 다른 스킬(힐링 등)은 순서에 상관없이 배울 수 있습니다. 따라서 스파크 →...","categories": ["PS"],
         "tags": ["programmers","level2","구현"],
-        "url": "https://ubermen5che.github.io/ps/PS-programmers-%EC%8A%A4%ED%82%AC%ED%8A%B8%EB%A6%AC/",
+        "url": "http://localhost:4000/ps/PS-programmers-%EC%8A%A4%ED%82%AC%ED%8A%B8%EB%A6%AC/",
         "teaser": null
       },{
         "title": "[PS-programmers] 괄호 변환",
         "excerpt":"문제 설명 카카오에 신입 개발자로 입사한 “콘”은 선배 개발자로부터 개발역량 강화를 위해 다른 개발자가 작성한 소스 코드를 분석하여 문제점을 발견하고 수정하라는 업무 과제를 받았습니다. 소스를 컴파일하여 로그를 보니 대부분 소스 코드 내 작성된 괄호가 개수는 맞지만 짝이 맞지 않은 형태로 작성되어 오류가 나는 것을 알게 되었습니다.수정해야 할 소스 파일이 너무 많아서...","categories": ["PS"],
         "tags": ["programmers","level2","재귀","구현"],
-        "url": "https://ubermen5che.github.io/ps/PS-programmers-%EA%B4%84%ED%98%B8-%EB%B3%80%ED%99%98/",
+        "url": "http://localhost:4000/ps/PS-programmers-%EA%B4%84%ED%98%B8-%EB%B3%80%ED%99%98/",
         "teaser": null
       },{
         "title": "[CS] Garbage Collection-1",
         "excerpt":"Garbage Collection이란? GC는 1959년에 개발된 자동적 메모리 관리 기법이다. GC란 이름을 보면 유추할 수 있듯이 여기서 말하는 Garbage는 사용되지 않는 Object를 말한다. GC는 결국 이러한 Garbage가 메모리에 있는지 scan하고 만약 존재한다면 다시 회수하는 동작을 하는 기법이라고 말할 수 있다. 개념적으로는 정말 간단해 보이지만 구현이 정말 까다롭다. GC는 발전을 거듭해서 꽤나...","categories": ["CS"],
         "tags": ["Java","CS","Reference counting","Mark & sweep","Generative Collection","Copy Collection"],
-        "url": "https://ubermen5che.github.io/cs/CS-Garbage-Collection-1/",
+        "url": "http://localhost:4000/cs/CS-Garbage-Collection-1/",
         "teaser": null
       },{
         "title": "[Programming Language] Syntax, Grammer",
         "excerpt":"사전지식 프로그램은 statements로 구성되어있다. Statement는 다음과 같이 구성됨. Expressions variables, operators and method calls Declarations Control flow Statement는 natural language에서 대략적으로 하나의 sentences와 같다. aValue = 3922.1234(assignment statement) Object myobjct = new Object(); (object creation statement) 하나의 statement forms은 프로그램 실행 단위이다. Introduction Syntax : expressions, statements, and program units의...","categories": ["Programming Language"],
         "tags": ["CS","Grammer","BNF","EBNF","Syntax","Derivation"],
-        "url": "https://ubermen5che.github.io/programming%20language/Programming-Language-Syntax-Grammer/",
+        "url": "http://localhost:4000/programming%20language/Programming-Language-Syntax-Grammer/",
+        "teaser": null
+      },{
+        "title": "[PS] 뱀(baekjoon 3190)",
+        "excerpt":"‘Dummy’ 라는 도스게임이 있다. 이 게임에는 뱀이 나와서 기어다니는데, 사과를 먹으면 뱀 길이가 늘어난다. 뱀이 이리저리 기어다니다가 벽 또는 자기자신의 몸과 부딪히면 게임이 끝난다. 게임은 NxN 정사각 보드위에서 진행되고, 몇몇 칸에는 사과가 놓여져 있다. 보드의 상하좌우 끝에 벽이 있다. 게임이 시작할때 뱀은 맨위 맨좌측에 위치하고 뱀의 길이는 1 이다. 뱀은 처음에 오른쪽을...","categories": ["PS"],
+        "tags": ["시뮬레이션","큐","덱"],
+        "url": "http://localhost:4000/ps/baekjoon-9190/",
         "teaser": null
       }]
